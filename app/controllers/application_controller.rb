ad06d2c355c39adfec2b32f03a5bc8f7bb36090f
YYYYNNNN002 <138284688+YYYYNNNN002@users.noreply.github.com>
2023-07-04 10:28:14 +0000

    Fix:新規登録画面・User修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, if: :use_auth?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def after_sign_in_path_for(resource)
-    root_path
+    users_path
   end
 
   def after_sign_out_path_for(resource)
@@ -14,5 +14,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+
+  def use_auth?
+    unless controller_name == 'homes'
+      true
+    end
   end
 end
