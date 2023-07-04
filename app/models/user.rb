d5c4eecfcc2e8ba399da87cee135840eed0ac8d7
YYYYNNNN002 <138284688+YYYYNNNN002@users.noreply.github.com>
2023-07-04 20:36:33 +0000

    start: Favorite概形作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 140 }
-
-
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
